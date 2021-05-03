d45e1908f29ebe9db6c6079e4cb59063b1ea1376
Christoph Mussenbrock <christoph@etherisc.com>
2021-05-03 13:04:12 +0200

    meteor-kitchen 202105031104120696

--- both/joins/joins.js
+++ both/joins/joins.js
@@ -1,5 +1,6 @@
 // Contracts
 Contracts.join(Chains, "chain_id", "chain_data", []);
+Contracts.join(Chains, "chain_data.name", "chain_data", []);
 Contracts.join(Chains, "deployed_at_block", "chain_data", []);
 
 // Addresses
