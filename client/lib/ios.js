00afa71419bbbe3b4f4a087f0e932a73fb086210
Christoph Mussenbrock <christoph@etherisc.com>
2021-05-07 14:29:56 +0200

    meteor-kitchen 202105071229566231

--- client/lib/ios.js
+++ client/lib/ios.js
@@ -17,38 +17,38 @@ function formatDate(date) {
 
 
 reloadProduct = async (productId) => {
-	
+
 	Meteor.call('product.reload', { productId });
-	
+
 }
 
 
 
+const handleError = (message, args) => {
+	alert(message),
+		error(message, args);
+}
+
+const handleInfo = (message, args) => {
+	alert(message),
+		info(message, args);
+}
+
+
+const ios = await getContract('InstanceOperatorService');
+const license = await getContract('License');	
+
+
+if (!ios || !license) {
+	handleError(`Could not create contract instances (ios/license)`);
+	return;
+}
+
+
 setProductPaused = async (productId, pause) => {
 
-
-	const handleError = (message, args) => {
-		alert(message),
-		error(message, args);
-	}
-	
-	const handleInfo = (message, args) => {
-		alert(message),
-		info(message, args);
-	}
-	
-	
-	
-	const ios = await getContract('InstanceOperatorService');
-	const license = await getContract('License');	
-	
-	if (!ios || !license) {
-		handleError(`Could not create contract instances (ios/license)`);
-		return;
-	}
-	
 	let product;
-	
+
 	try {
 		product = await license.products(productId);
 	} catch (err) {
@@ -57,7 +57,7 @@ setProductPaused = async (productId, pause) => {
 	}
 
 	info(`Trying to pause/unpause product ${productId}`, product);
-	
+
 	if(pause && !product.paused) {
 		try {
 			const res = await ios.pauseProduct(productId)
@@ -80,6 +80,34 @@ setProductPaused = async (productId, pause) => {
 		}
 	} else {
 		handleInfo(`Product ${product.name} already in state <${product.paused ? 'paused' : 'unpaused'}>`);
+	}
+}
+
+approveProduct = async (productId) => {
+
+	let product;
+
+	try {
+		product = await license.products(productId);
+	} catch (err) {
+		handleError(`Could not access product ${productId}; (${err.message})`);
+		return;
+	}
+
+	info(`Trying to approve product ${productId}`, product);
+
+	if(!product.approved) {
+		try {
+			const res = await ios.approveProduct(productId)
+			info(`Transaction submitted`, res);
+			const receipt = await res.wait();
+			info(`Transaction confirmed`, receipt);
+			await reloadProduct(productId);
+		} catch (err) {
+			handleError(`${err.message} ${err.data ? `Code: ${err.data.code} ${err.data.message}` : ''}`, err);
+		}
+	} else {
+		handleInfo(`Product ${product.name} already approved`);
 	}
 }
 
