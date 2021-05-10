17a0a4159f378716d89d342043cdee83fd0a3087
Christoph Mussenbrock <christoph@etherisc.com>
2021-05-10 16:45:01 +0200

    meteor-kitchen 202105101445014510

---
+++ client/views/policies_public/policies_public_controller.js
@@ -0,0 +1,67 @@
+this.PoliciesPublicController = RouteController.extend({
+	template: "PoliciesPublic",
+	
+
+	yieldTemplates: {
+		/*YIELD_TEMPLATES*/
+	},
+
+	onBeforeAction: function() {
+		this.next();
+	},
+
+	action: function() {
+		if(this.isReady()) { this.render(); } else { this.render("loading"); }
+		/*ACTION_FUNCTION*/
+	},
+
+	isReady: function() {
+		this.policyListPagedExtraParams = {
+			searchText: Session.get("PolicyListPagedSearchString") || "",
+			searchFields: Session.get("PolicyListPagedSearchFields") || ["policy_id", "metadata_id", "state", "state_message", "created_at", "updated_at", "metadata_mongo_id"],
+			sortBy: Session.get("PolicyListPagedSortBy") || "",
+			sortAscending: Session.get("PolicyListPagedSortAscending"),
+			pageNo: Session.get("PolicyListPagedPageNo") || 0,
+			pageSize: Session.get("PolicyListPagedPageSize") || 0
+		};
+
+
+
+		
+
+		var subs = [
+			Meteor.subscribe("policy_list_paged", this.policyListPagedExtraParams),
+			Meteor.subscribe("policy_list_paged_count", this.policyListPagedExtraParams)
+		];
+		var ready = true;
+		_.each(subs, function(sub) {
+			if(!sub.ready())
+				ready = false;
+		});
+		return ready;
+	},
+
+	data: function() {
+		
+
+		var data = {
+			params: this.params || {},
+			policy_list_paged: Policies.find(databaseUtils.extendFilter({}, this.policyListPagedExtraParams), databaseUtils.extendOptions({}, this.policyListPagedExtraParams)),
+			policy_list_paged_count: Counts.get("policy_list_paged_count")
+		};
+		
+
+		
+		data.policy_list_paged_page_count = this.policyListPagedExtraParams && this.policyListPagedExtraParams.pageSize ? Math.ceil(data.policy_list_paged_count / this.policyListPagedExtraParams.pageSize) : 1;
+		if(this.isReady() && this.policyListPagedExtraParams.pageNo >= data.policy_list_paged_page_count) {
+			Session.set("PolicyListPagedPageNo", data.policy_list_paged_page_count > 0 ? data.policy_list_paged_page_count - 1 : 0);
+		}
+
+
+		return data;
+	},
+
+	onAfterAction: function() {
+		
+	}
+});
