a5ffd5bc4cb0fc63995d6c3981766ebe61bc0a68
Christoph Mussenbrock <christoph@etherisc.com>
2021-05-10 18:19:43 +0200

    meteor-kitchen 202105101619432435

--- client/views/applications/applications.js
+++ client/views/applications/applications.js
@@ -254,7 +254,7 @@ Template.ApplicationsViewTableItems.events({
 		var itemId = item ? item._id : null;
 
 		
-		Router.go("applications_public.details", mergeObjects(Router.currentRouteParams(), {applicationId: this._id}));
+		Router.go("applications.details", mergeObjects(Router.currentRouteParams(), {applicationId: this._id}));
 		return false;
 	},
 
