320aabd548e6b7a52335c2fbadcc779fa312e07d
Christoph Mussenbrock <christoph@etherisc.com>
2021-07-15 10:00:34 +0200

    meteor-kitchen 202107150800346150

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -2,34 +2,9 @@ console.log('loading helpers.js');
 
 import { ethers } from 'ethers';
 import { bpDoc  } from '/client/lib/bp-doc.js';
-import {
-	b32s,
-	s32b,
-	stateMessage,
-	mapHeader,
-	mapVal,
-	json2TableHtml,
-	json2Table,
-	array2TableHtml,
-	array2Table,
-	abi2Table,
-	pre,
-	safeStr,
-} from '/client/lib/utils.js';
+import utils from '/client/lib/utils.js';
 
-Helpers = {};
-
-Helpers.pre = pre;
-Helpers.safeStr = safeStr;
-Helpers.json2Table = json2Table;
-Helpers.array2Table = array2Table;
-Helpers.abi2Table = abi2Table;
-Helpers.bpDoc = bpDoc;
-
-Helpers.ipfsLink = (ipfs) => safeStr(`<a href="https://gateway.pinata.cloud/ipfs/${ipfs.ipfs}" target="_blank">/ipfs/${ipfs.ipfs}</a>`);
-Helpers.txLink = (txHash) => safeStr(`<a href="https://blockscout.com/xdai/mainnet/tx/${txHash}" target="_blank">${txHash.slice(0,10)}...</a>`);
-Helpers.addressLink = (address) => safeStr(`<a href="https://blockscout.com/xdai/mainnet/address/${address}" target="_blank">${address.slice(0,10)}...</a>`);
-Helpers.addressLongLink = (address) => safeStr(`<a href="https://blockscout.com/xdai/mainnet/address/${address}" target="_blank">${address}</a>`);
+Helpers = { ...utils };
 	
 Helpers.productState = (state) => stateMessage.product[state];
 Helpers.oracleState = (state) => stateMessage.oracle[state];
