4e2862ec1cd93a743f789511e16fbfb4154f553c
Christoph Mussenbrock <christoph@etherisc.com>
2021-07-19 13:56:24 +0200

    meteor-kitchen 202107191156243429

--- client/views/oracles/oracles.js
+++ client/views/oracles/oracles.js
@@ -26,7 +26,7 @@ Template.Oracles.helpers({
 var OraclesViewExport = function(fileType) {
 	var extraParams = {
 		searchText: Session.get("OracleListPagedSearchString") || "",
-		searchFields: Session.get("OracleListPagedSearchFields") || ["oracle_id", "description", "oracle_contract", "oracle_owner", "active_oracle_types"],
+		searchFields: Session.get("OracleListPagedSearchFields") || ["oracle_id", "description", "oracle_contract", "oracle_owner", "activated", "active_oracle_types"],
 		sortBy: Session.get("OracleListPagedSortBy") || "",
 		sortAscending: Session.get("OracleListPagedSortAscending") || true
 	};
