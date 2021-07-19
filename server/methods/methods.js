ef3d7c35152d08a5be6abc53f757577eb3ebf6fe
Christoph Mussenbrock <christoph@etherisc.com>
2021-07-20 00:50:09 +0200

    meteor-kitchen 202107192250091796

--- server/methods/methods.js
+++ server/methods/methods.js
@@ -3,7 +3,15 @@ import logger from '/imports/server/methods/logger.js';
 
 import { reloadContracts, getContract } from '/imports/server/methods/gif-contracts-reader.js';
 import { reloadEvents } from '/imports/server/methods/gif-logs-reader.js';
-import { loadOracles, reloadOracles, loadOracleTypes, reloadOracleTypes, getAssignedOracles, getAssignedOracleTypes } from '/imports/server/methods/gif-oracles-reader.js';
+import { 
+	loadOracles, 
+	reloadOracles, 
+	loadOracleTypes, 
+	reloadOracleTypes, 
+	getAssignedOracles, 
+	getAssignedOracleTypes, 
+	getUnassignedOracleTypes 
+} from '/imports/server/methods/gif-oracles-reader.js';
 import { reloadProducts, reloadSingleProduct } from '/imports/server/methods/gif-products-reader.js';
 import { getPolicies, reloadPolicies } from '/imports/server/methods/gif-policies-reader.js';
 import { bpData } from '/imports/server/methods/bp-data.js';
