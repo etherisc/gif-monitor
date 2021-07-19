63ecb5a6e5971c436a373f11ecec2bae5b5252ae
Christoph Mussenbrock <christoph@etherisc.com>
2021-07-19 23:55:38 +0200

    meteor-kitchen 202107192155381217

--- imports/server/methods/gif-oracles-reader.js
+++ imports/server/methods/gif-oracles-reader.js
@@ -11,10 +11,10 @@ const loadOracles = async() => {
 	try {
 		const Query = await getContract('Query');
 
-		const oracleIdIncrement = await Query.oracleIdIncrement();
-		info(`${ oracleIdIncrement - 1 } Oracles found`);
+		const oracleCount = await Query.oracleCount();
+		info(`${ oracleCount - 1 } Oracles found`);
 
-		for (var oracleIndex = 1; oracleIndex < oracleIdIncrement; oracleIndex += 1) {
+		for (var oracleIndex = 1; oracleIndex < oracleCount; oracleIndex += 1) {
 			const oracle = await Query.oracles(oracleIndex);
 
 			info(`Found oracle #${oracleIndex} (${oracle.description})`, { oracle });
@@ -41,12 +41,12 @@ const loadOracleTypes = async() => {
 
 	try {
 		const Query = await getContract('Query');
-		const oracleTypeNamesIncrement = await Query.oracleTypeNamesIncrement();
-		info(`${ oracleTypeNamesIncrement - 1 } OracleTypes found`);
-		const oracleIdIncrement = await Query.oracleIdIncrement();
-		info(`${ oracleIdIncrement - 1 } Oracles found`);
+		const oracleTypeNamesCount = await Query.oracleTypeNamesCount();
+		info(`${ oracleTypeNamesCount - 1 } OracleTypes found`);
+		const oracleIdCount = await Query.oracleIdCount();
+		info(`${ oracleIdCount - 1 } Oracles found`);
 
-		for (var oracleTypeIndex = 1; oracleTypeIndex < oracleTypeNamesIncrement; oracleTypeIndex += 1) {
+		for (var oracleTypeIndex = 1; oracleTypeIndex < oracleTypeNamesCount; oracleTypeIndex += 1) {
 			const oracleTypeNameB32 = await Query.oracleTypeNames(oracleTypeIndex);
 			const oracleTypeName = b32s(oracleTypeNameB32);
 			const oracleType = await Query.oracleTypes(oracleTypeNameB32);
@@ -54,7 +54,7 @@ const loadOracleTypes = async() => {
 			info(`Found oracleType ${oracleTypeName}`, { oracleType });
 			
 			const assignedOracles = [];
-			for (var idx = 1; idx < oracleIdIncrement; idx += 1) {
+			for (var idx = 1; idx < oracleIdCount; idx += 1) {
 				const assignmentState = await Query.assignedOracles(oracleTypeNameB32, idx);
 				if (assignmentState > 0) {
 					assignedOracles.push({oracleId: idx, assignmentState});
